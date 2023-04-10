925e625a2473473a91e5466fcd0468132c548c28
Jake Harvey <jacob.harvey@mail.mcgill.ca>
2023-04-09 21:00:49 -0400

    Set up package stuff

--- src/EcologicalHypergraphs.jl
+++ src/EcologicalHypergraphs.jl
@@ -1,11 +1,25 @@
+module EcologicalHypergraphs
+
 using MacroTools: prewalk, postwalk, unblock, rmlines
 using EcologicalNetworks
 using ModelingToolkit
 using Symbolics
 using LinearAlgebra
 
-include("types/declarations.jl")
-include("types/conversions.jl")
-include("predicates.jl")
-include("functional_form.jl")
-include("build_system.jl")
+include(joinpath(".", "types", "declarations.jl"))
+export EcologicalHypergraph, Node, Edge, 
+       species, role, nodes, subject, 
+       object, modifiers, add_modifier!
+
+include(joinpath(".", "types", "conversions.jl"))
+
+include(joinpath(".", "predicates.jl"))
+export isloop, contains
+
+include(joinpath(".", "functional_form.jl"))
+export @functional_form
+
+include(joinpath(".", "build_system.jl"))
+export community_matrix, build_system
+
+end
