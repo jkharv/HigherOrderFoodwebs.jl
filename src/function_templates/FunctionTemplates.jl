5ffca1b3d1d6dadb90814800797ee61ecc40b528
Jake Harvey <jacob.harvey@mail.mcgill.ca>
2023-07-11 16:39:19 -0400

    Fixed warning about @functional_form on package load

--- src/function_templates/FunctionTemplates.jl
+++ src/function_templates/FunctionTemplates.jl
@@ -3,5 +3,3 @@ include(joinpath(".", "types.jl"))
 include(joinpath(".", "parsing.jl"))
 include(joinpath(".", "functional_form.jl"))
 include(joinpath(".", "function_building.jl"))
-
-export @functional_form
