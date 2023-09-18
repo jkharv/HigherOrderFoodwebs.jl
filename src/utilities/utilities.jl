e11056c78dbddc2f30454ded4c7c786799692ca3
Jake Harvey <jacob.harvey@mail.mcgill.ca>
2023-09-18 18:04:04 -0400

    added `remove!`

--- src/utilities/utilities.jl
+++ src/utilities/utilities.jl
@@ -7,3 +7,60 @@ function string_to_var(hg::EcologicalHypergraph, s::String)
     return var
 end
 
+function remove!(hg::EcologicalHypergraph, sp::String)
+
+    names = map(x -> x.species, nodes(hg)) 
+
+    for i in eachindex(names)
+
+        if sp ∈ names[i]
+
+            remove!(nodes(hg)[i])
+        end
+    end
+
+    remove_orphan_species!(hg)
+end
+
+function remove!(node::Node)
+
+    edge = node.edge.value
+
+    if length(nodes(edge)) <= 2
+
+        remove!(edge)
+    end
+
+    for i ∈ eachindex(nodes(edge))
+
+        deleteat!(nodes(edge), i)
+        break
+    end
+
+    remove_orphan_species!(edge.hypergraph.value)
+end
+
+function remove!(edge::Edge)
+
+    hg = edge.hypergraph.value
+
+    for i ∈ eachindex(interactions(hg))
+
+        if interactions(hg)[i] == edge
+           
+            deleteat!(interactions(hg), i)
+            break
+        end
+    end
+
+    remove_orphan_species!(hg)
+end
+
+function remove_orphan_species!(hg::EcologicalHypergraph)
+
+    names = map(x -> x.species, nodes(hg))
+    names = collect(Iterators.flatten(names))
+    unique!(names)
+
+    hg.species = sort(names)
+end
