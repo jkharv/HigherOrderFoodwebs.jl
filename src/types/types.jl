b1f496a0c63a34cefee21e50619e77190b9e2223
Jake Harvey <jacob.harvey@mail.mcgill.ca>
2024-06-12 16:12:41 -0400

    no need for this dumb lil hierarchy of includes

--- src/types/types.jl
+++
@@ -1,5 +0,0 @@
-# The allowable types for giving values to parameters and variables.
-const TermValue = Union{Missing, T, W} where {T<:Real, W<:UnivariateDistribution}
-
-include("foodweb_model.jl")
-include("community_matrix.jl")
