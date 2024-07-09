da69d503340965efc5d16a0b5152a27072481e14
Jake Harvey <jacob.harvey@mail.mcgill.ca>
2024-07-09 11:59:41 -0400

    predicate for testing if consumer & producer

--- src/interfaces/foodweb_interface.jl
+++ src/interfaces/foodweb_interface.jl
@@ -11,3 +11,26 @@ function set_initial_condition!(fwm::FoodwebModel{T}, u0::Dict{T, Float64}) wher
         fwm.u0[b] = u0[k]
     end
 end
+
+function isproducer(fwm::FoodwebModel, sp)::Bool
+
+    @assert sp ∈ species(fwm)
+
+    for intx ∈ interactions(fwm)
+
+        if isloop(intx)
+
+            continue
+        elseif subject(intx) == sp
+
+            return false
+        end
+    end
+
+    return true
+end
+
+function isconsumer(fwm::FoodwebModel, sp)::Bool
+
+    return !isproducer(fwm, sp)
+end
