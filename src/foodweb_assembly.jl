d3819ab9fb9a9b4159e5af7957cfad7894748905
Jake Harvey <jacob.harvey@mail.mcgill.ca>
2024-09-16 14:12:15 -0400

    have `assemble_foodweb` output a new `FoodwebModel` rather than a solver.

--- src/foodweb_assembly.jl
+++ src/foodweb_assembly.jl
@@ -1,6 +1,6 @@
 const LOW_DENSITY = 0.0001;
 
-function assemble_foodweb!(fwm::FoodwebModel)
+function assemble_foodweb(fwm::FoodwebModel)
 
     invasion_sequence = trophic_ordering(fwm)
 
@@ -21,5 +21,31 @@ function assemble_foodweb!(fwm::FoodwebModel)
         step!(integrator, 100.0)
     end
 
-    return integrator
+    u0 = Dict{Num, Number}()
+
+    # Species
+    for (s, v) in fwm.vars
+
+        u0[v] = integrator.integrator.sol[v][end]
+    end
+
+    # Aux vars
+    for (s, v) in fwm.aux_vars
+
+        u0[v] = integrator.integrator.sol[s][end]
+    end
+
+    return FoodwebModel(
+        fwm.hg,
+        fwm.dynamic_rules,
+        fwm.t,
+        fwm.vars,
+        u0,
+        fwm.params,
+        fwm.param_vals,
+        fwm.aux_dynamic_rules,
+        fwm.aux_vars,
+        fwm.odes,
+        fwm.community_matrix
+    )
 end
