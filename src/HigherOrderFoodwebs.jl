95ea772df9b40acda13aefd8955dbd6ff9bb5744
Jake Harvey <jacob.harvey@mail.mcgill.ca>
2025-03-19 12:21:26 -0400

    formatting

--- src/HigherOrderFoodwebs.jl
+++ src/HigherOrderFoodwebs.jl
@@ -11,9 +11,9 @@ using SciMLBase
 using OrdinaryDiffEq
 using SymbolicIndexingInterface
 
-#
-# Core type declrations
-#
+# ----------------------#
+# Core type declrations #
+# ----------------------#
 
 include("./types/dynamic_rule.jl")
 export DynamicRule
@@ -28,9 +28,10 @@ export FoodwebModel
 include("./types/community_matrix.jl")
 export CommunityMatrix
 
-#
-# Core interfaces
-#
+# --------------- #
+# Core interfaces #
+# --------------- #
+
 include("./core_interfaces/dynamic_rule.jl")
 
 include("./core_interfaces/foodweb_variables.jl")
@@ -45,15 +46,15 @@ export get_symbol, get_variable, set_u0!
 export add_var!, add_param!
 
 include("./core_interfaces/community_matrix.jl")
-# The Array interface from Base
+# The Array interface from Base. No exports needed
 
 include("./core_interfaces/sciml_ext.jl")
 export ODESystem, ODEProblem
 
+# --------------------------------------------------------- #
+# Solver callbacks that are generally relevant for foodwebs #
+# --------------------------------------------------------- #
 
-#
-# Solver callbacks that are generally relevant for foodwebs
-#
 include("./solver_callbacks/extinction_sequence_callback.jl")
 export ExtinctionSequenceCallback
 
@@ -63,32 +64,35 @@ export ExtinctionThresholdCallback
 include("./solver_callbacks/richness_termination_callback.jl")
 export RichnessTerminationCallback
 
-#
-# Foodweb assembly models
-#
+# ----------------------- #
+# Foodweb assembly models #
+# ----------------------- #
+
 include("./foodweb_assembly/foodweb_assembly.jl")
 include("./foodweb_assembly/trophic_ordering.jl")
 export assemble_foodweb
 
-#
-# Structural models
-#
+# ----------------- #
+# Structural models #
+# ----------------- #
+
 include("./structural_models/types.jl")
 include("./structural_models/optimal_foraging.jl")
 include("./structural_models/nichemodel.jl")
 export nichemodel, optimal_foraging
 
+# --------------------------------------------------------- #
+# Common functions and their derivatives for foodweb models #
+# --------------------------------------------------------- #
 
-#
-# Common functions and their derivatives for foodweb models
-#
 include("./registered_symbolics/holling_2.jl")
 include("./registered_symbolics/logistic.jl")
 export holling2, logistic
 
-#
-# Tools for examining realized networks
-#
+# ------------------------------------- #
+# Tools for examining realized networks #
+# ------------------------------------- #
+
 include("realized_networks/trophic_flux.jl")
 export trophic_flux
 
