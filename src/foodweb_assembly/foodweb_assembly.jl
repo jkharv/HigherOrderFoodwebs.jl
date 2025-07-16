a3d8a4a812d627983875b9d7ac18814fc48a5e23
Jake Harvey <jacob.harvey@mail.mcgill.ca>
2025-07-15 22:17:55 -0400

    `assemble_foodweb` now takes an `ODEProblem`

--- src/foodweb_assembly/foodweb_assembly.jl
+++ src/foodweb_assembly/foodweb_assembly.jl
@@ -1,91 +1,37 @@
-const LOW_DENSITY = 0.1;
-
-function assemble_foodweb(fwm::FoodwebModel, solver = AutoTsit5(Rosenbrock23()); 
+function assemble_foodweb(prob::ODEProblem; 
+    solver = AutoTsit5(Rosenbrock23()), 
     extra_transient_time = 0,
-    compile_symbolics = true,
-    compile_jacobian = false,
-    kwargs...
+    time_between_invasions = 100,
+    invader_density = 0.1,
+    extinction_threshold = 1e-20
 )
 
-    defaults = (
-        maxiters = 1e7,
-        force_dtmin = true,
-        save_on = false,
-        reltol = 1e-3,
-        abstol = 1e-3
-    )
-    kwargs = merge(defaults, kwargs) 
-    
-    prob = ODEProblem(
-        fwm, 
-        (1, 100 * richness(fwm) + 200); 
-        compile_symbolics,
-        compile_jacobian
-    )
+    if !(prob.f.sys isa FoodwebModel)
 
-    u0 = introduce_species(fwm, prob, solver; extra_transient_time, kwargs...)
-
-    return remake(prob; u0 = u0)
-end
-
-function assemble_stochastic_foodweb(fwm::FoodwebModel, solver = SOSRA(); 
-    extra_transient_time = 0,
-    compile_symbolics = true,
-    compile_jacobian = false,
-    kwargs...
-)
-
-    defaults = (
-        maxiters = 1e7,
-        force_dtmin = true,
-        save_on = false,
-        reltol = 1e-3,
-        abstol = 1e-3
-    )
-    kwargs = merge(defaults, kwargs) 
-    
-    prob = SDEProblem(
-        fwm, 
-        default_noise(fwm, 0.01), 
-        (1, 100 * richness(fwm) + 200); 
-        compile_symbolics,
-        compile_jacobian
-    )
-
-    u0 = introduce_species(fwm, prob, solver; extra_transient_time, kwargs...)
-
-    return remake(prob; u0 = u0)
-end
-
-
-function introduce_species(fwm, prob, solver; extra_transient_time, kwargs...)
-
-    invasion_sequence = trophic_ordering(fwm)
-    cb = ExtinctionThresholdCallback(fwm, 1e-20)
-
-    integrator = init(prob, solver;
-        callback = cb, 
-        kwargs...
-    );   
-
-    while !isempty(invasion_sequence)
-
-        sp = popfirst!(invasion_sequence)
-        integrator[sp] = LOW_DENSITY
-
-        # Recalculate the derivatives to account for discontinuity.
-        u_modified!(integrator, true)
-        step!(integrator, 100)
+        error("ODEProblem must be created with HigherOrderFoodwebs.jl")
     end
 
-    step!(integrator, extra_transient_time)
+    fwm = prob.f.sys
 
-    return integrator.u
-end
+    invasion_sequence = trophic_ordering(fwm);
 
-function merge_args(defaults, user)
+    n_invasions = length(invasion_sequence)
+    tend_invasions = time_between_invasions * n_invasions
+    tend = tend_invasions + extra_transient_time
+    invasion_times = collect(1:time_between_invasions:tend_invasions)
+    tspan = (1, tend)
 
-    # I need to deal with Callbacks seperately
-    # I'll do that later tho. (maybe)
-    return merge(user, defaults)
+    is = InvasionSequenceCallback(fwm, invasion_sequence, invasion_times;
+        invader_density = invader_density,
+    )
+    et = ExtinctionThresholdCallback(fwm, extinction_threshold)
+
+    sol = solve(prob, solver, 
+        callback = CallbackSet(is, et),
+        tspan = tspan,
+        tstops = invasion_times,
+        saveat = tend 
+    )
+
+    return remake(prob, u0 = sol[end])
 end
